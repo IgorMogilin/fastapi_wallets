24da27c91fb0108c6a11558352f7844d9aabdec1
Igor Mogilin <endless8space@gmail.com>
2025-09-29 01:45:27 +0300

    Create wallets model

---
+++ app/models/wallets.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from decimal import Decimal
+from typing import Optional
+from uuid import UUID
+
+from sqlalchemy import DateTime, Numeric, String, Text
+from sqlalchemy import UUID as UUID_field
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.database import Base
+
+
+class Wallet(Base):
+    """Модель кошелька."""
+
+    __tablename__ = 'wallets'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[Optional[str]] = mapped_column(
+        Text(500),
+        nullable=True,
+    )
+    uuid: Mapped[UUID] = mapped_column(
+        UUID_field(as_uuid=True),
+        nullable=False,
+        unique=True
+    )
+    balance: Mapped[Decimal] = mapped_column(Numeric(10, 2), default=0.00)
+    executing_date: Mapped[Optional[datetime]] = mapped_column(
+        DateTime,
+        nullable=True,
+    )
