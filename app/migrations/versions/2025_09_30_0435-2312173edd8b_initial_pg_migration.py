9059c05d1feacb25424eb39c83657d4067cd8444
Igor Mogilin <endless8space@gmail.com>
2025-10-01 02:40:14 +0300

    Fix readme and env

--- app/migrations/versions/2025_09_30_0435-2312173edd8b_initial_pg_migration.py
+++ app/migrations/versions/2025_09_30_0435-2312173edd8b_initial_pg_migration.py
@@ -43,7 +43,7 @@ def upgrade() -> None:
            """).bindparams(
                title="Основной кошелек",
                description="Автоматически созданный кошелек",
-               uuid=uuid.uuid4(),
+               uuid=uuid.UUID("3fa85f64-5717-4562-b3fc-2c963f66afa6"),
                balance=1000.00,
                executing_date=datetime.now()
            )
