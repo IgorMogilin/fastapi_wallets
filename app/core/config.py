70535ce47bf635b1b79ced007724c30336288c4d
Igor Mogilin <endless8space@gmail.com>
2025-09-30 04:38:48 +0300

    Fix initial migration

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Для тестов используем SQLite, для продакшена PostgreSQL
 if os.getenv("TESTING") == "true":
     DATABASE_URL = "sqlite+aiosqlite:///:memory:"
 else:
